25854b717f682329e3fbfc7de97cb2209a43bda0
Eeshan Malwandikar <144798063+eeshan03@users.noreply.github.com>
2024-01-09 12:52:34 +0530

    Update oeeserver.js
    ip changed from 101 to 26

--- oeeserver.js
+++ oeeserver.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 
 // connection configurations
 const dbConn = mysql.createConnection({
-  host: '192.168.1.101',
+  host: '192.168.1.26',
   user: 'Dhruv',
   password: '1234',
   port: 3306,
