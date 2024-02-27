f735f69b51d634533d6d2b75b17905d1b74f43f1
Eeshan Malwandikar <144798063+eeshan03@users.noreply.github.com>
2024-02-27 12:40:16 +0530

    Update Sidebar2.js

--- src/components/Sidebar2.js
+++ src/components/Sidebar2.js
@@ -9,6 +9,9 @@ import "./resizing.css";
 import ImageResizer from "react-image-resizer";
 import Header from "./Header";
 
+// Sidebar.js and SidebarData.js are for Superadmin
+// Sidebar2.js and SidebarData2.js are for Manager
+
 const Nav = styled.div`
   background: #c62828;
   height: 50px;
